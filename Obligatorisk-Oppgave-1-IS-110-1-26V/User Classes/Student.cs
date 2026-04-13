905aa00906be0f25203fe92a29a3f0c838d161b3
JovnnaAlex <jaguttorm@uia.no>
2026-04-13 03:37:43 +0200

    Refactor Student class to remove StudentId
    Removed StudentId property and updated constructor parameters. Adjusted PrintUserInfo method to reflect changes.

--- Obligatorisk-Oppgave-1-IS-110-1-26V/User Classes/Student.cs
+++ Obligatorisk-Oppgave-1-IS-110-1-26V/User Classes/Student.cs
@@ -5,23 +5,26 @@ namespace Application;
 public class Student : User
 {
     // Deklarerer verdier med auto-imp
-    public string StudentId {get; private set; } //ID skal låses etter objektet er laget
+    // public string StudentId {get; private set; } //ID skal låses etter objektet er laget. **Trenger ikke denne, da vi har Id i User klassen**
     public string StudyProgram {get; set; }
     // Konstruktøren 
-    public Student(string name, 
-                   string email, 
-                   string studentId, 
+    public Student(string id, 
+                   string name, 
+                   string email,
+                   string username,
+                   string password, 
                    string studyProgram)
+        // : base = lager student objekt etter user klassen har kjørt sin konstruktør.
+        // Vi kaller "protected user" 
+        : base(id, name, email, username, password, role: "Student")
     {
-        Name = name;               // fra User klassen
-        Email = email;             // fra User klassen
-        StudentId = studentId;     // Lager ekstra parameter til denne klassen
         StudyProgram = studyProgram; 
     }
     public override void PrintUserInfo() // Erstatter den abstrakte metoden i UserAbstract, og returnerer "Student" for denne klassen
     {
          base.PrintUserInfo(); // Kaller den virtuelle metoden i User klassen for å skrive ut navn og e-post
-        Console.WriteLine($"Student ID: {StudentId}, Study Program: {StudyProgram}"); // Skriver ut studentens ID og studieprogram, Navn og e-post kommer fra User klassen
+        Console.WriteLine($"Study Program: {StudyProgram}"); // Skriver ut studentens studieprogram + "Student" for denne klassen, Navn og e-post kommer fra User klassen
+
     }
 }
     
