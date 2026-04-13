c28dde146d7712c40e486775edac1bdb09735b64
JovnnaAlex <jaguttorm@uia.no>
2026-04-13 03:40:15 +0200

    Update ExchangeStudent.cs

--- Obligatorisk-Oppgave-1-IS-110-1-26V/User Classes/ExchangeStudent.cs
+++ Obligatorisk-Oppgave-1-IS-110-1-26V/User Classes/ExchangeStudent.cs
@@ -1,12 +1,12 @@
 using System;
 
 namespace Application;
-//Arver fra User
-public class ExchangeStudent : User
+//Arver fra student
+public class ExchangeStudent : Student
 {
     // Deklarerer verdier med auto-imp
-    public string ExStudentId { get; private set; } //ID skal låses etter objektet er laget
-    public string ExStudyProgram { get; set; }
+    // public string ExStudentId { get; private set; } //ID skal låses etter objektet er laget *trenger ikke*
+    // public string ExStudyProgram { get; set; } *trenger ikke, da vi har StudyProgram i Student klassen*
     public string HomeUniversity { get; set; }
     public string Country { get; set; }
     public DateTime PeriodFrom { get; set; }
@@ -14,22 +14,20 @@ public class ExchangeStudent : User
 
 
     // Konstruktøren 
-    public ExchangeStudent(string name,
-                           string email,
-                           string studentId,
+    public ExchangeStudent(string id, 
+                           string name, 
+                           string email, 
+                           string username,
+                           string password,
                            string studyProgram,
                            string homeUniversity,
                            string country,
                            DateTime periodFrom,
                            DateTime periodTo)
-                          
-        
-        
+        // : base = lager exchange student objekt etter student klassen har kjørt sin konstruktør
+        : base(id, name, email, username, password, studyProgram)
+
     {
-        Name = name;               // fra User klassen
-        Email = email;             // fra User klassen
-        ExStudentId = studentId;   // Lager ekstra parameter til denne klassen
-        ExStudyProgram = studyProgram;
         HomeUniversity = homeUniversity;
         Country = country;
         PeriodFrom = periodFrom;
@@ -38,7 +36,7 @@ public class ExchangeStudent : User
      public override void PrintUserInfo() // Erstatter den abstrakte metoden i UserAbstract, og returnerer "Student" for denne klassen
     {
         base.PrintUserInfo(); // Kaller den virtuelle metoden i User klassen for å skrive ut navn og e-post
-        Console.WriteLine($"Exchange Student ID: {ExStudentId}, Exchange Study Program: {ExStudyProgram}, Home University: {HomeUniversity}, Country: {Country}, Period: {PeriodFrom} to {PeriodTo}"); // Skriver ut exchange studentens ID og informasjon, Navn og e-post kommer fra User klassen
+        Console.WriteLine($"Home University: {HomeUniversity}, Country: {Country}, Period: {PeriodFrom} to {PeriodTo}"); // Skriver ut exchange studentens ID og informasjon, Navn og e-post kommer fra User klassen og studieprogram kommer fra Student klassen
     }
 }
 
